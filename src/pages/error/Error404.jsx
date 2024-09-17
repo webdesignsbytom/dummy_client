547fcacddb61594df21897845e9d59c0481a75e7
xtombrock <xtombrock1989@gmail.com>
2024-09-17 22:37:40 +0100

    error page updated

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -18,26 +18,31 @@ function Error404() {
           <Navbar />
 
           {/* Main page content */}
-          <main role='main' className='grid w-full h-full overflow-hidden'>
-            <section className='grid w-full text-center h-full items-center justify-center z-10'>
-              <div className='grid gap-2'>
-
-                {/* Dialog */}
-                <article className='outline outline-2 font-semibold outline-black -mt-10 p-2 rounded'>
-                  <h1 className='text-4xl'>
-                    ERROR <span className='text-error-red'>404</span>
-                  </h1>
-                  <h2 className='text-4xl'>PAGE NOT FOUND</h2>
+          <main
+            role='main'
+            className='relative grid w-full h-full overflow-hidden'
+          >
+            <section className='grid absolute w-full text-center h-full items-center justify-center z-10'>
+              <section className='grid gap-2'>
+                <article className='outline outline-2 font-semibold outline-black -mt-10 rounded'>
+                  <div className='p-4'>
+                    <h1 className='text-4xl text-left'>
+                      ERROR <span className='text-red-500'>404</span>
+                    </h1>
+                    <h2 className='text-4xl'>PAGE NOT FOUND</h2>
+                  </div>
+                  <article className='text-red-500 text-left pl-4 border-t-2 border-solid border-black p-2'>
+                    <h3 className='uppercase'>But you found a friend 💖</h3>
+                  </article>
                 </article>
-
-                <article className='outline outline-2 font-semibold outline-black rounded'>
-                  <h3>But you found a friend 💖</h3>
-                </article>
-              </div>
+              </section>
             </section>
 
             <section className='flex justify-end lg:mr-20'>
-              <img src={CatNotFound} alt='Lost cat for error page that is funny and cute.' />
+              <img
+                src={CatNotFound}
+                alt='Lost cat for error page that is funny and cute.'
+              />
             </section>
           </main>
         </div>
