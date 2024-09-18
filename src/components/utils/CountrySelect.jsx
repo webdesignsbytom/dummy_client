9b66c5a66a71ed290b1f1ad2f202d7c45be5206b
xtombrock <xtombrock1989@gmail.com>
2024-09-18 18:48:19 +0100

    save

--- src/components/utils/CountrySelect.jsx
+++ src/components/utils/CountrySelect.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // Styles
-import { InputStyle } from '../Styles';
+import { InputStyle } from '../../utils/Styles';
 
 function CountrySelect({ handleChange }) {
   return (
