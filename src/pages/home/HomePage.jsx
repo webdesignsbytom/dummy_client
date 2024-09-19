a1d6c40ad015e8cbf08d75940383ecbf23fc1ffa
xtombrock <xtombrock1989@gmail.com>
2024-09-19 18:13:06 +0100

    save

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,11 +3,9 @@ import React, { lazy, Suspense } from 'react';
 import { usePageTracking } from '../../hooks/useAnalytics';
 // Components
 import Navbar from '../../components/nav/Navbar';
-import SocialMediaAuth from '../../components/socialMedia/SocialMediaAuth';
 import { HelmetItem } from '../../components/utils/HelmetItem';
 // Constants
 import { CompanyName } from '../../utils/Constants';
-import SocialMediaCTA from '../../components/socialMedia/SocialMediaCTA';
 
 // Lazy-loaded ChatBotComponent
 const ChatBotComponent = lazy(() =>
@@ -16,37 +14,6 @@ const ChatBotComponent = lazy(() =>
 
 const HomePage = React.memo(() => {
   usePageTracking(); // Tracks page views
-
-  const handleLoginWith = (service) => {
-    switch (service) {
-      case 'facebook':
-        // Call Facebook login API
-        console.log('Logging in with Facebook');
-        break;
-      case 'instagram':
-        // Call Instagram login API
-        console.log('Logging in with Instagram');
-        break;
-      case 'google':
-        // Call Google login API
-        console.log('Logging in with Google');
-        break;
-      case 'github':
-        // Call GitHub login API
-        console.log('Logging in with GitHub');
-        break;
-      case 'apple':
-        // Call Apple login API
-        console.log('Logging in with Apple');
-        break;
-      case 'x':
-        // Call X login API
-        console.log('Logging in with X');
-        break;
-      default:
-        console.log('Unsupported service');
-    }
-  };
 
   return (
     <>
@@ -67,10 +34,6 @@ const HomePage = React.memo(() => {
           {/* Main page content */}
           <main role='main'>
             <div>home</div>
-            <SocialMediaAuth func={handleLoginWith} text={'Login with'} />
-            <div>
-              <SocialMediaCTA />
-            </div>
           </main>
         </div>
       </div>
