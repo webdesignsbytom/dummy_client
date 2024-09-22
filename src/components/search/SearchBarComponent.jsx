d5d128d84e373f3c7f608a4a4ed11852a37f986f
Thomas Brockington <xtombrock1989@gmail.com>
2024-09-22 11:51:41 +0100

    working style

--- src/components/search/SearchBarComponent.jsx
+++ src/components/search/SearchBarComponent.jsx
@@ -5,16 +5,16 @@ import { FaSearch } from 'react-icons/fa';
 function SearchBarComponent() {
   return (
     <div className='bg-slate-400 p-2'>
-      <div className='bg-white grid max-w-xl p-1 rounded-lg shadow-xl grid-cols-reg gap-1'>
+      <div className='bg-white grid grid-cols-reg max-w-xl p-1 rounded-lg shadow-xl gap-1'>
         <div className='grid items-center justify-center px-1'>
-          <div className='grid overflow-hidden'>
+          <div className='grid overflow-hidden min-w-max'>
             <FaSearch />
           </div>
         </div>
         <input
           type='text'
           placeholder='Search...'
-          className='active:outline-none focus:outline-none'
+          className='active:outline-none focus:outline-none min-w-10'
         />
       </div>
     </div>
