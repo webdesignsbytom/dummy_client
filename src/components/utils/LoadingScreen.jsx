3b7c086fbf6198fcb3b8fe09e3c7cac037df7146
xtombrock <xtombrock1989@gmail.com>
2024-09-24 18:38:23 +0100

    loading screen nav bar

--- src/components/utils/LoadingScreen.jsx
+++ src/components/utils/LoadingScreen.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 // Components
 import LoadingSpinner from './LoadingSpinner';
+import Navbar from '../nav/Navbar';
 
 function LoadingScreen() {
   return (
-    <div 
-      id='loading-screen' 
-      className='grid h-screen w-full overflow-hidden justify-center items-center'
-      aria-live='polite' 
+    <div
+      id='loading-screen'
+      className='grid h-screen w-full overflow-hidden'
+      aria-live='polite'
       aria-busy='true'
       role='alert'
       aria-labelledby='loading-description'
     >
-      <article className='grid gap-2 h-fit'>
-        <div>
-          {/* Providing a descriptive aria label for screen readers */}
-          <p id='loading-description'>The content is loading, please wait...</p>
-        </div>
-        <div className='grid justify-center'>
-          {/* Spinner for visual feedback */}
-          <LoadingSpinner lg={true} />
-        </div>
-      </article>
+      <div className='grid grid-rows-reg'>
+        <Navbar />
+
+        <main className='grid justify-center items-center'>
+          <article className='grid gap-2 h-fit'>
+            <div>
+              {/* Providing a descriptive aria label for screen readers */}
+              <p id='loading-description'>
+                The content is loading, please wait...
+              </p>
+            </div>
+            <div className='grid justify-center'>
+              {/* Spinner for visual feedback */}
+              <LoadingSpinner lg={true} />
+            </div>
+          </article>
+        </main>
+      </div>
     </div>
   );
 }
