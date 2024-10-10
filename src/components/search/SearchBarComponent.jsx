dcc799a0402c45b8fb5a1689493b5a2c5ebcd7bc
Thomas Brockington <xtombrock1989@gmail.com>
2024-10-10 19:22:03 +0100

    save

--- src/components/search/SearchBarComponent.jsx
+++ src/components/search/SearchBarComponent.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 // Icons
 import { FaSearch } from 'react-icons/fa';
 
+const searchTerms = ['hello', 'world', 'cat', 'book', 'cheese'];
+
 function SearchBarComponent() {
+  const [items, setItems] = useState(searchTerms);
   const [searchQuery, setSearchQuery] = useState('');
+
   console.log('searchQuery', searchQuery);
 
-  const handleSearchChange = (event) => {
-    const { value } = event.target;
-    setSearchQuery(value);
-  };
+  const filteredItems = useMemo(() => {
+    return items.filter(item => {
+      return item.toLowerCase().includes(searchQuery.toLowerCase())
+    })
+  }, [items, searchQuery]) 
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
@@ -18,27 +23,37 @@ function SearchBarComponent() {
   };
 
   const handleSubmit = () => {
+    if (searchQuery === '') return;
+
     console.log('Search submitted with query: ', searchQuery);
   };
 
   return (
-    <div className='bg-slate-400 p-2'>
-      <div className='bg-white grid grid-cols-reg max-w-xl p-1 rounded-lg shadow-xl gap-1'>
-        <div className='grid items-center justify-center px-1'>
-          <div className='grid overflow-hidden min-w-max cursor-pointer'>
-            <FaSearch onClick={handleSubmit} />
+    <>
+      <div className='bg-slate-400 p-2'>
+        <div className='bg-white grid grid-cols-reg max-w-xl p-1 rounded-lg shadow-xl gap-1'>
+          <div className='grid items-center justify-center px-1'>
+            <div className='grid overflow-hidden min-w-max cursor-pointer'>
+              <FaSearch onClick={handleSubmit} />
+            </div>
           </div>
+          <input
+            type='search'
+            placeholder='Search...'
+            className='active:outline-none focus:outline-none min-w-10'
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={handleKeyPress} // Listening for Enter key
+            value={searchQuery}
+          />
         </div>
-        <input
-          type='text'
-          placeholder='Search...'
-          className='active:outline-none focus:outline-none min-w-10'
-          onChange={handleSearchChange}
-          onKeyDown={handleKeyPress} // Listening for Enter key
-          value={searchQuery}
-        />
       </div>
-    </div>
+
+      <div>
+        {filteredItems.map((item) => {
+          return <div>{item}</div>;
+        })}
+      </div>
+    </>
   );
 }
 
